2d962b1f884a81520b34efdb2397f48ee22115ea
Kyle Harding <kyle@balena.io>
2026-07-19 11:31:52 -0400

    Add Flowzone CI with versionist versioning and plugin-zip publish
    Version the plugin via a committed versionist.conf.js that bumps the version
    string in marketplace.json, wired through Flowzone. A custom publish action
    zips the plugin as a gh-release-* artifact for the org admin panel.
    
    Signed-off-by: Kyle Harding <kyle@balena.io>

---
+++ versionist.conf.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const execSync = require('child_process').execSync;
+
+const getAuthor = (commitHash) => {
+  return execSync(`git show --quiet --format="%an" ${commitHash}`, {
+    encoding: 'utf8'
+  }).replace('\n', '');
+};
+
+const isIncrementalCommit = (changeType) => {
+  return Boolean(changeType) && changeType.trim().toLowerCase() !== 'none';
+};
+
+module.exports = {
+  // This setup allows the editing and parsing of footer tags to get version and type information,
+  // as well as ensuring tags of the type 'v<major>.<minor>.<patch>' are used.
+  // It increments in a semver compatible fashion and allows the updating of NPM package info.
+  editChangelog: true,
+  parseFooterTags: true,
+  getGitReferenceFromVersion: 'v-prefix',
+  incrementVersion: 'semver',
+  updateVersion: (cwd, version, callback) => {
+    const fs = require('fs');
+    const file = `${cwd}/.claude-plugin/marketplace.json`;
+    const updated = fs
+      .readFileSync(file, 'utf8')
+      .replace(/("version":\s*")[^"]*(")/, `$1${version}$2`);
+    fs.writeFileSync(file, updated);
+    callback();
+  },
+
+  // Always add the entry to the top of the Changelog, below the header.
+  addEntryToChangelog: {
+    preset: 'prepend',
+    fromLine: 6
+  },
+
+  // Only include a commit when there is a footer tag of 'change-type'.
+  // Ensures commits which do not up versions are not included.
+  includeCommitWhen: (commit) => {
+    return isIncrementalCommit(commit.footer['change-type']);
+  },
+
+  // Determine the type from 'change-type:' tag.
+  // Should no explicit change type be made, then no changes are assumed.
+
+  getIncrementLevelFromCommit: (commit) => {
+    const match = commit.subject.match(/^(patch|minor|major):/i);
+    if (Array.isArray(match) && isIncrementalCommit(match[1])) {
+      return match[1].trim().toLowerCase();
+    }
+
+    if (isIncrementalCommit(commit.footer['change-type'])) {
+      return commit.footer['change-type'].trim().toLowerCase();
+    }
+  },
+
+  // If a 'changelog-entry' tag is found, use this as the subject rather than the
+  // first line of the commit.
+  transformTemplateData: (data) => {
+    data.commits.forEach((commit) => {
+      commit.subject = commit.footer['changelog-entry'] || commit.subject;
+      commit.author = getAuthor(commit.hash);
+    });
+
+    return data;
+  },
+
+  template: [
+    '## v{{version}} - {{moment date "Y-MM-DD"}}',
+    '',
+    '{{#each commits}}',
+    '{{#if this.author}}',
+    '* {{capitalize this.subject}} [{{this.author}}]',
+    '{{else}}',
+    '* {{capitalize this.subject}}',
+    '{{/if}}',
+    '{{/each}}'
+  ].join('\n')
+};
